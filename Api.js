fd3bbedb1f8f9231199871cf2971ae6acaed4b69
ComposeVenture <composeventures@gmail.com>
2024-07-28 09:17:23 +0300

    Add files via upload

--- Api.js
+++ Api.js
@@ -1,3 +1,7 @@
 const EMAILAPI='https://script.google.com/macros/s/AKfycbzcO9ISVLPMUpRkYUAwHPHaLxchT0fWNxQx48Pq4ixY5-v0EWk3aI9tpmDqYlMl7DTK/exec';
 
 const Email='wamangav@gmail.com';
+
+const POSTAPI='https://script.google.com/macros/s/AKfycbw7AHMLEYUiEY6oT-wGPHWZSS5Rvqs9l9pdV0jTUtCj3KB47eQ0lq8Wl85VXhE1LfM3/exec'
+
+const GETDATA='https://script.google.com/macros/s/AKfycbxhvVn7RvEONr3oBiDNOYOGQ-JPpRwEme0QuSK1qqHVAjX5IK9FA4UxmUtGJdsO3GK6/exec';
